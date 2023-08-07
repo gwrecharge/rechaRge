cb8dddb2f1e2e5354ac0366366f5f61992339f65
Yannick Marcon <yannick.marcon@epfl.ch>
2023-08-07 15:09:57 +0200

    feat:  compute_hydrobudget is split in sub functions (wip) (#7)
    * feat: compute_hydrobudget is split in sub functions (wip)
    
    * feat: calibration script moved temporarily in the examples folder
    
    * feat: compute_simulation_quality_assessment added
    
    * feat: write_results added

--- R/utils.R
+++ R/utils.R
@@ -67,3 +67,39 @@ process_river_flow <- function(observed_flow, alpha_lyne_hollick) {
     gauging = gauging
   )
 }
+
+#' Make the computation cluster
+#' @keywords internal
+.make_cluster <- function(nb_core) {
+  cluster <- makeCluster(nb_core) # if on a computer with windows
+  registerDoParallel(cluster) # if on a computer with windows
+  # registerDoMC(nb_core)        #if on a server - DoParallel does not work, use that instead (doMC)
+  cluster
+}
+
+#' Stop the computation cluster
+#' @keywords internal
+.stop_cluster <- function(cluster) {
+  stopCluster(cluster) # to be muted if doMC is used instead of doParallel
+}
+
+#' Create a new progress instance with default settings.
+#' @import progress
+#' @keywords internal
+.newProgress <- function(format = "  :what [:bar] :current/:total | :elapsed", clear = getOption("recharge.progress.clear", !interactive()), total, width = 100) {
+  pb <- progress::progress_bar$new(format = format, clear = clear, total = total, width = width, show_after = 0)
+  pb$tick(0, tokens = list(what = ''))
+  pb
+}
+
+#' Update and increment the progress status if option "recharge.progress" is TRUE.
+#' @keywords internal
+.tickProgress <- function(progress, tokens = list()) {
+  if (getOption("recharge.progress", interactive())) progress$tick(tokens = tokens)
+}
+
+#' Update the progress status if option "recharge.progress" is TRUE.
+#' @keywords internal
+.updateProgress <- function(progress, step, total, tokens = list()) {
+  if (getOption("recharge.progress", interactive())) progress$update(ratio = step/total, tokens = tokens)
+}
--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onLoad <- function(libname, pkgname) {
-  utils::globalVariables(c("X_L93", "Y_L93", "cell_ID", "climate_cell", "..cols", ".SD", "VI", "j", "k", "pet", "rcn_cell", "t_mean", "Q", "bf_lh"))
+  utils::globalVariables(c("X_L93", "Y_L93", "cell_ID", "climate_cell", "..cols", ".SD", "VI", "j", "k", "pet", "rcn_cell", "t_mean", "Q", "bf_lh", "delta_reservoir", "runoff", "runoff_2", "aet", "gwr"))
 }
